21baa7addc710c49ff8375108f1eb4ab7fdb3679
HARSHAVARDHAN REDDY KOMMAREDDY <80662558+Harsha13-os@users.noreply.github.com>
2025-01-24 20:01:16 -0500

    Time: 0 ms (100.00%) | Memory: 9.6 MB (90.13%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s == t) return true;
-        return false;
+        int freq[26] = {0};
+        for(int i=0; i<s.length(); i++){
+            freq[s[i]-'a']++;
+        }
+        for(int i=0; i<t.length(); i++){
+            freq[t[i]-'a']--;
+        }
+        for(int j=0; j<26; j++){
+            if(freq[j] != 0) return false;
+        }
+        return true;
     }
 };
