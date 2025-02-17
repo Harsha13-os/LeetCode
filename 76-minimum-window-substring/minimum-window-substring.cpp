d602f7dfdf0921ae043edc50dcf9359031196da1
HARSHAVARDHAN REDDY KOMMAREDDY <80662558+Harsha13-os@users.noreply.github.com>
2025-02-17 15:14:04 -0500

    Time: 3 ms (80.96%) | Memory: 11.8 MB (11.85%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        if (s.empty() || t.empty()) return "";
+
+        int l=0, r=0, minl = INT_MAX;
+        int hash[256] = {0};
+        int count = 0, index = -1;
+        for(int i=0; i<t.length(); i++){
+            hash[t[i]]++;
+        }
+
+        while(r<s.length()){
+            if(hash[s[r]]>0){
+                count++;
+            }
+            hash[s[r]]--;
+
+            while(count == t.length()){
+                if(r-l+1 < minl){
+                    minl = r-l+1;
+                    index = l;
+                }
+                hash[s[l]]++;
+                if(hash[s[l]]>0) count--;
+                l++;
+            }
+            r++;
+        }
+        return index == -1 ? "":s.substr(index, minl);
+    }
+};
