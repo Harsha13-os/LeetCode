e9e9e0781c6ecf34aca79219db9eb66ccb3f7b4e
HARSHAVARDHAN REDDY KOMMAREDDY <80662558+Harsha13-os@users.noreply.github.com>
2025-02-26 16:00:20 -0500

    Time: 2 ms (33.51%) | Memory: 11.4 MB (20.43%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -14,7 +14,8 @@ public:
     vector<int> countBits(int n) {
         vector<int> ans;
         for(int i=0; i<=n; i++){
-            ans.push_back(setBits(i));
+            // ans.push_back(setBits(i));
+            ans.push_back(__builtin_popcount(i));
         }
         return ans;
     }
